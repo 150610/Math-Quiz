40f309b6b3ab3340e9985507459469f09ed6a614
150610 <henryp4211@masseyhigh.school.nz>
2026-03-23 09:35:45 +1300

    file generates basic facts questions
    addition/subtraction is 0 - 100
    division is 1 - 50
    multiplication is 0 - 12

---
+++ generate_basic_facts_questions.py
@@ -0,0 +1,89 @@
+import random
+
+def int_check(question,):
+
+    # if not an integer
+    error = "Please enter an integer"
+
+    while True:
+        response = input(question).lower()
+
+        if response == 'xxx':
+            return response
+
+        else:
+            print(error)
+
+
+def generate_basic_add_sub_problem():
+    # Generate random numbers
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    operator = random.choice(['+', '-'])
+
+    # Create the question and calculate answer
+    question = f"{num1} {operator} {num2}"
+    # eval() calculates string expressions
+    answer = eval(question)
+
+    return question, answer
+
+def generate_basic_division_problem():
+    # Generate random numbers
+    num1 = random.randint(1, 50)
+    num2 = random.randint(1, 50)
+    operator = '/'
+
+    # make sure division will give whole numbers
+    if operator == '/':
+        # make num1 divisible by num2
+        num1 = num1 * num2
+
+    # Create the question and calculate answer
+    question = f"{num1} {operator} {num2}"
+    # eval() calculates string expressions
+    answer = eval(question)
+
+    return question, answer
+
+def generate_basic_multiplication_problem():
+    # Generate random numbers
+    num1 = random.randint(0, 12)
+    num2 = random.randint(0, 12)
+    operator = '*'
+
+    # Create the question and calculate answer
+    question = f"{num1} {operator} {num2}"
+    # eval() calculates string expressions
+    answer = eval(question)
+
+    return question, answer
+
+
+# test question for add/sub
+quest, ans = generate_basic_add_sub_problem()
+user_answer = int_check(f"What is {quest}? ")
+
+if user_answer == ans:
+    print("Correct!")
+else:
+    print(f"Wrong. The answer is {ans}")
+
+
+# test question for division
+quest, ans = generate_basic_division_problem()
+user_answer = int_check(f"What is {quest}? ")
+
+if user_answer == ans:
+    print("Correct!")
+else:
+    print(f"Wrong. The answer is {ans}")
+
+# test question for multiplication
+quest, ans = generate_basic_multiplication_problem()
+user_answer = int_check(f"What is {quest}? ")
+
+if user_answer == ans:
+    print("Correct!")
+else:
+    print(f"Wrong. The answer is {ans}")
